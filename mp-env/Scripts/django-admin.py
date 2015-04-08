14fb22b0b18c5af4f1cba538317424f4bb339b1b
diegofaria <diegofndefaria@gmail.com>
2015-04-08 01:57:29 -0300

    initial commit

---
+++ mp-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Diego\Documents\GitHub\recruiter-mp\mp-env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
