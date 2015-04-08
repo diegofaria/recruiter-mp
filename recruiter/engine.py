2d75dcc56a782f74838d695c92d461fd32c82025
diegofaria <diegofndefaria@gmail.com>
2015-04-08 02:17:45 -0300

    engine refactor for better readability

--- recruiter/engine.py
+++ recruiter/engine.py
@@ -4,10 +4,19 @@ class NewCandidateEngine(object):
 		self.open_positions = []
 
 	def getJobPositions(self, candidate):
-		if (candidate.html >= 7) and (candidate.css >= 7) and (candidate.javascript >= 7):
+		if self.is_front_end_developer(candidate):
 			self.open_positions.append('front_end')
-		if (candidate.python >= 7) and (candidate.django >= 7):
+		if self.is_back_end_developer(candidate):
 			self.open_positions.append('back_end')
-		if (candidate.ios >= 7) and (candidate.android >= 7):
+		if self.is_mobile_developer(candidate):
 			self.open_positions.append('mobile')
 		return self.open_positions
+
+	def is_front_end_developer(self, candidate):
+		return (candidate.html >= 7) and (candidate.css >= 7) and (candidate.javascript >= 7)
+
+	def is_back_end_developer(self, candidate):
+		return (candidate.python >= 7) and (candidate.django >= 7)
+
+	def is_mobile_developer(self, candidate):
+		return (candidate.ios >= 7) and (candidate.android >= 7)
